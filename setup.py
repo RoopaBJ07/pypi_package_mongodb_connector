42c7ad64a227c91e41e114da1674fffe8d774d85
Roopa <roopabj@roopas-MacBook-Air.local>
2025-11-21 07:55:33 +0530

    Fixed setup and successful local build

--- setup.py
+++ setup.py
@@ -1,31 +1,16 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT = '-e .'
-
-def get_requiremet(file_path: str) -> list[str]:
-    requirements = []
-    
-    with open(file_path) as f:
-        requirements = f.readlines()
-    
-    requirements = [req.replace("\n", "") for req in requirements]
-
-    if HYPEN_E_DOT in requirements:
-        requirements.remove(HYPEN_E_DOT)
-    
-    return requirements
-
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
-
    
 
 __version__ = "2.20.0"
 REPO_NAME = "pypi_package_mongodb_connector"
-PKG_NAME= "MOngo_connect"
+PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "Roopa_BJ"
 AUTHOR_EMAIL = "roopajagadish.1999@gmail.com"
+
 
 setup(
     name=PKG_NAME,
@@ -41,5 +26,4 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requirement("requirements_dev.txt")
     )
